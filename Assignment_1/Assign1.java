34bbcb7ae7046c5bff037290f1586e287902c245
Gabriel Nielsen <Gxnielsen@gmail.com>
2026-01-08 21:04:21 -0700

    arguments

--- Assignment_1/Assign1.java
+++ Assignment_1/Assign1.java
@@ -19,16 +19,16 @@ public class Assign1 {
         if (n < 0 || n > 40) {
             throw new IllegalArgumentException("fibonacci value must be between 0 and 40");
         }
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1;
-        } else if (n == 1) {
-            return 1;
-        } else {
+        }  else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        for (int i = 0; i <= args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
     }
 }
