6000974089fc2c9e3f715fd5bd329a5185b637d6
Gabriel Nielsen <Gxnielsen@gmail.com>
2026-01-12 17:59:56 -0700

    final fix of everything

--- Assignment_1/Assign1.java
+++ Assignment_1/Assign1.java
@@ -1,20 +1,27 @@
-
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 
+/**
+ * Assign1 class provides methods to compute factorial, Fibonacci numbers,
+ * and the mathematical constant e using series expansion.
+ * @author Gabriel Nielsen 
+ */
 public class Assign1 {
+
+    // Scale is set to 25 to give a more accurate e calculation
+    // while stopping a non-terminating decimal expansion
+    private static final int SCALE = 25;
 
     /**
      * Computes the factorial of a given non-negative integer.
-     * @author Gabriel Nielsen
      * @param value the non-negative integer to compute the factorial for
      * @return the factorial of the given integer as a BigInteger
      * @throws IllegalArgumentException if the input value is negative
      */
     private static BigInteger factorial(int value) {
         if (value < 0) {
-            throw new IllegalArgumentException("factorial valid range: [0, " + Integer.MAX_VALUE + "]");
+            throw new IllegalArgumentException("Factorial valid range is [0, " + Integer.MAX_VALUE + "]");
         }
         BigInteger total = BigInteger.valueOf(1);
 
@@ -26,14 +33,13 @@ public class Assign1 {
 
     /**
      * Computes the nth Fibonacci number using recursion.
-     * @author Gabriel Nielsen
      * @param value the index of the Fibonacci number to compute (0 <= value <= 40)
      * @return the nth Fibonacci number
      * @throws IllegalArgumentException if the input value is negative or greater than 40
      */
     private static int fibonacci(int value) {
         if (value < 0 || value > 40) {
-            throw new IllegalArgumentException("fibonacci valid range: [0, 40]");
+            throw new IllegalArgumentException("Fibonacci valid range is [0, 40]");
         }
         if (value == 0 || value == 1) {
             return 1;
@@ -44,18 +50,16 @@ public class Assign1 {
     
     /**
      * Computes the mathematical constant e using a series expansion.
-     * @author Gabriel Nielsen
      * @param value the number of terms to include in the series (value > 0)
      * @return the computed value of e as a BigDecimal
      * @throws IllegalArgumentException if the input value is less than 1
      */
     private static BigDecimal eSeries(int value) {
         if (value < 1) {
-            throw new IllegalArgumentException("Valid e iteration range: [1, " + Integer.MAX_VALUE + "]");
+            throw new IllegalArgumentException("Valid e iteration range is [1, " + Integer.MAX_VALUE + "]");
         }
         BigDecimal sum = BigDecimal.ONE;
         BigDecimal term = BigDecimal.ONE;
-        int SCALE = 25;
 
         for (int i = 1; i < value; i++) {
             term = term.divide(BigDecimal.valueOf(i), SCALE, RoundingMode.HALF_UP);
@@ -66,7 +70,7 @@ public class Assign1 {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("-- Assign1 Info --");
+            System.out.println("-- Assign 1 Help --");
             System.out.println(" -fib [n] : Compute the nth Fibonacci number (0 <= n <= 40)");
             System.out.println(" -fac [n] : Compute the factorial of n (n >= 0)");
             System.out.println(" -e [n] : Compute the value of e using n iterations (n > 0)");
